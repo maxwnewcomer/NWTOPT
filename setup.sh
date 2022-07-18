ab69c35bbab85d3dfe439f3684e3dbcf11b4608d
Christopher Vanden Boom <cvandenboom2@gmail.com>
2022-07-18 14:57:57 -0500

    Fixed bash script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
-echo "Enter an environment name:"
+#!/bin/bash
+echo "Enter an environment name"
 read envname
+echo "Environment name will be $envname"
 read -p "Press enter to create new environment $envname"
 conda env create -n $envname -f conda_requirements.yml
 read -p "Press enter to activate $envname"
@@ -10,4 +11,5 @@ conda pack -n $envname -o nwtenv.tgz
 read -p "Press enter to create necessary directories"
 mkdir mongodb/db
 mkdir logs
-mkdir logs/condor_logs; mkdirgit stlogs/errors; mkdir logs/outputs
+mkdir logs/condor_logs; mkdir gitlogs/errors; mkdir logs/outputs
+echo "Completed directory and environment setup for NWTOPT"
